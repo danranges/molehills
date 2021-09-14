c3383c4fd136f75d51aaa6f00c4f0fd7cca61f41
Dan Ranges <danranges@gmail.com>
2021-09-14 20:20:31 +0200

    Add tasks class

--- src/tasks.js
+++ src/tasks.js
@@ -1,8 +1,16 @@
 
+class Task {
+    constructor(name, desc, project, status, due) {
+        this.name = name
+        this.desc = desc
+        this.project = project
+        this.status = status
+        this.due = due
+    }
 
-function addTask() {
+}
+
+export default function addTask() {
     const workspace = document.getElementById('workspace')
     workspace.innerHTML = 'test'
 }
-
-export default addTask
