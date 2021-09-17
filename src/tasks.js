980a37443af208572b78e976d251cd193ff6ccf0
Dan Ranges <danranges@gmail.com>
2021-09-17 21:05:28 +0200

    Add new task form

--- src/tasks.js
+++ src/tasks.js
@@ -28,10 +28,46 @@ function addEditTaskCard(name = 'Task',
                         status = false) {
                                 
     const card = document.createElement('div')
+    
     const cardForm = document.createElement('form')
+    cardForm.classList.add('new-task-form')
 
+    const taskName = document.createElement('input')
+    taskName.setAttribute('type', 'text')
+    taskName.setAttribute('name', 'TaskName')
+    taskName.setAttribute('placeholder', name)
+    
+    const taskDesc = document.createElement('input')
+    taskDesc.setAttribute('type', 'text')
+    taskDesc.setAttribute('name', 'TaskDescription')
+    taskDesc.setAttribute('placeholder', desc)
 
-    card.appendChild(cardForm)
+    const taskProj = document.createElement('input')
+    taskProj.setAttribute('type', 'text')
+    taskProj.setAttribute('name', 'TaskProject')
+    taskProj.setAttribute('placeholder', project)
+    
+	const taskStatus = document.createElement('input')
+    taskStatus.setAttribute('type', 'checkbox')
+    taskStatus.setAttribute('name', 'TaskStatus')
+    taskStatus.setAttribute('value', 'Status')
+	taskStatus.id = 'task-status-checkbox'
+
+	const taskStatusLabel = document.createElement('label')
+	taskStatusLabel.htmlFor = 'task-status-checkbox'
+	taskStatusLabel.appendChild(taskStatus)
+	
+	const formSubmit = document.createElement('input')
+	formSubmit.setAttribute('type', 'submit')
+	formSubmit.setAttribute('value', 'Submit')
+
+	card.appendChild(cardForm)
+	cardForm.appendChild(taskName)
+	cardForm.appendChild(taskDesc)
+	cardForm.appendChild(taskProj)
+	cardForm.appendChild(taskStatusLabel)
+	cardForm.appendChild(formSubmit)
+	
 
 }
 
