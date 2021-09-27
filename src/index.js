12656dc06784f758627fe7037e712f768e2bbb86
Dan Ranges <danranges@gmail.com>
2021-09-27 21:13:03 +0200

    Add bottom nav events

--- src/index.js
+++ src/index.js
@@ -1,58 +1,60 @@
-import './styles.css'
-import Logo from '../Assets/mole.svg'
-import addTask from './tasks'
+import './styles.css';
+import Logo from '../Assets/mole.svg';
+import addTask from './tasks';
+import renderProjects from './project';
 
 (function render() {
+  function createHeader() {
+    const header = document.createElement('div');
+    header.classList.add('header');
 
-    function createHeader() {
-        const header = document.createElement('div')
-        header.classList.add('header')
+    const hdrLogo = new Image();
+    hdrLogo.src = Logo;
+    hdrLogo.classList.add('header-icon');
 
-        const hdrLogo = new Image()
-        hdrLogo.src = Logo
-        hdrLogo.classList.add('header-icon')
+    const hdrTxt = document.createElement('div');
+    hdrTxt.innerHTML = 'Molehills';
+    hdrTxt.classList.add('header-text');
 
-        const hdrTxt = document.createElement('div')
-        hdrTxt.innerHTML = 'Molehills'
-        hdrTxt.classList.add('header-text')
+    header.appendChild(hdrLogo);
+    header.appendChild(hdrTxt);
 
-        header.appendChild(hdrLogo)
-        header.appendChild(hdrTxt)
+    return header;
+  }
 
-        return header
-    }
+  function createWorkspace() {
+    const workspace = document.createElement('div');
+    workspace.id = 'workspace';
+    workspace.classList.add('workspace');
+    return workspace;
+  }
 
-    function createWorkspace() {
-        const workspace = document.createElement('div')
-        workspace.id = 'workspace'
-        workspace.classList.add('workspace')
-        return workspace
-    }
+  function createNav() {
+    const nav = document.createElement('div');
+    nav.classList.add('nav');
 
-    function createNav() {
-        const nav = document.createElement('div')
-        nav.classList.add('nav')
+    const btnHome = document.createElement('button');
+    btnHome.classList.add('btn-home');
+    btnHome.addEventListener('click', renderHome);
 
-        const btnHome = document.createElement('button')
-        btnHome.classList.add('btn-home')
+    const btnAddTask = document.createElement('button');
+    btnAddTask.classList.add('btn-add-task');
+    btnAddTask.addEventListener('click', addTask);
 
-        const btnAddTask = document.createElement('button')
-        btnAddTask.classList.add('btn-add-task')
-        btnAddTask.addEventListener('click', addTask)
+    const btnMenu = document.createElement('button');
+    btnMenu.classList.add('btn-menu');
+    btnMenu.addEventListener('click', renderProjects);
 
+    nav.appendChild(btnHome);
+    nav.appendChild(btnAddTask);
+    nav.appendChild(btnMenu);
 
-        const btnMenu = document.createElement('button')
-        btnMenu.classList.add('btn-menu')
+    return nav;
+  }
 
-        nav.appendChild(btnHome)
-        nav.appendChild(btnAddTask)
-        nav.appendChild(btnMenu)
+  document.body.appendChild(createHeader());
+  document.body.appendChild(createWorkspace());
+  document.body.appendChild(createNav());
+})();
 
-        return nav
-    }
-
-    document.body.appendChild(createHeader())
-    document.body.appendChild(createWorkspace())
-    document.body.appendChild(createNav())
-})()
-
+function renderHome() {}
