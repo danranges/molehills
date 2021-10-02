cc7c222fe22c4266c34fc2400c308b3465e40060
Dan Ranges <danranges@gmail.com>
2021-10-02 16:45:07 +0200

    Refactor UI building module

---
+++ src/UI.js
@@ -0,0 +1,121 @@
+import Logo from '../Assets/mole.svg';
+
+export default class UI {
+  static render() {
+    function createHeader() {
+      const header = document.createElement('div');
+      header.classList.add('header');
+
+      const hdrLogo = new Image();
+      hdrLogo.src = Logo;
+      hdrLogo.classList.add('header-icon');
+
+      const hdrTxt = document.createElement('div');
+      hdrTxt.innerHTML = 'Molehills';
+      hdrTxt.classList.add('header-text');
+
+      header.appendChild(hdrLogo);
+      header.appendChild(hdrTxt);
+
+      return header;
+    }
+
+    function createWorkspace() {
+      const workspace = document.createElement('div');
+      workspace.id = 'workspace';
+      workspace.classList.add('workspace');
+
+      return workspace;
+    }
+
+    function createNav() {
+      const nav = document.createElement('div');
+      nav.classList.add('nav');
+
+      const btnHome = document.createElement('button');
+      btnHome.classList.add('btn-home');
+      //   btnHome.addEventListener('click', renderHome);
+
+      const btnAddTask = document.createElement('button');
+      btnAddTask.classList.add('btn-add-task');
+      btnAddTask.addEventListener('click', UI.addEditTaskCard);
+
+      const btnMenu = document.createElement('button');
+      btnMenu.classList.add('btn-menu');
+      //   btnMenu.addEventListener('click', renderProjects);
+
+      nav.appendChild(btnHome);
+      nav.appendChild(btnAddTask);
+      nav.appendChild(btnMenu);
+
+      return nav;
+    }
+
+    document.body.appendChild(createHeader());
+    document.body.appendChild(createWorkspace());
+    document.body.appendChild(createNav());
+  }
+
+  static addEditTaskCard(
+    name = 'Task',
+    desc = 'Description',
+    project = 'Project',
+    status = true,
+    addEdit = 'Add Task',
+  ) {
+    const workspace = document.getElementById('workspace');
+
+    const overlay = document.createElement('div');
+    overlay.classList.add('new-task-overlay');
+
+    const card = document.createElement('div');
+    card.classList.add('new-task-card');
+
+    const cardForm = document.createElement('form');
+    cardForm.id = 'new-task-form';
+    cardForm.classList.add('new-task-form');
+
+    const taskName = document.createElement('input');
+    taskName.setAttribute('type', 'text');
+    taskName.setAttribute('name', 'TaskName');
+    taskName.setAttribute('placeholder', name);
+
+    const taskDesc = document.createElement('textarea');
+    taskDesc.setAttribute('type', 'text');
+    taskDesc.setAttribute('name', 'TaskDescription');
+    taskDesc.setAttribute('placeholder', desc);
+    taskDesc.setAttribute('rows', 5);
+
+    const taskProj = document.createElement('input');
+    taskProj.setAttribute('type', 'text');
+    taskProj.setAttribute('name', 'TaskProject');
+    taskProj.setAttribute('placeholder', project);
+
+    const taskStatus = document.createElement('input');
+    taskStatus.setAttribute('type', 'checkbox');
+    taskStatus.setAttribute('name', 'TaskStatus');
+    taskStatus.setAttribute('value', status);
+    taskStatus.id = 'task-status-checkbox';
+
+    const taskStatusLabel = document.createElement('label');
+    taskStatusLabel.classList.add('task-status-label');
+    taskStatusLabel.htmlFor = 'task-status-checkbox';
+    taskStatusLabel.innerHTML = 'Completed?';
+    taskStatusLabel.appendChild(taskStatus);
+
+    const formSubmit = document.createElement('input');
+    formSubmit.setAttribute('type', 'submit');
+    formSubmit.setAttribute('value', addEdit);
+
+    workspace.appendChild(overlay);
+    overlay.appendChild(card);
+    card.appendChild(cardForm);
+    cardForm.appendChild(taskName);
+    cardForm.appendChild(taskProj);
+    cardForm.appendChild(taskDesc);
+    cardForm.appendChild(taskStatusLabel);
+    cardForm.appendChild(formSubmit);
+
+    // cardForm.addEventListener('submit', appendNewTask);
+  }
+}
--- src/tasks.js
+++ src/tasks.js
@@ -1,6 +1,6 @@
 let taskList = [];
 
-class Task {
+export default class Task {
   constructor(name, desc, project, status) {
     this.name = name;
     this.project = project;
@@ -10,7 +10,7 @@ class Task {
   }
 }
 
-export default function addTask() {
+function addTask() {
   clearWorkspace();
   addEditTaskCard();
 }
@@ -43,71 +43,10 @@ function appendNewTask(e) {
   renderWorkspace();
 }
 
-function renderWorkspace(project = '*') {}
+function renderWorkspace(project = '*') {
+  console.log(taskList);
+}
 
 function clearWorkspace() {
   workspace.innerHTML = '';
-}
-
-function addEditTaskCard(
-  name = 'Task',
-  desc = 'Description',
-  project = 'Project',
-  status = true,
-  addEdit = 'Add Task',
-) {
-  const workspace = document.getElementById('workspace');
-
-  const overlay = document.createElement('div');
-  overlay.classList.add('new-task-overlay');
-
-  const card = document.createElement('div');
-  card.classList.add('new-task-card');
-
-  const cardForm = document.createElement('form');
-  cardForm.id = 'new-task-form';
-  cardForm.classList.add('new-task-form');
-
-  const taskName = document.createElement('input');
-  taskName.setAttribute('type', 'text');
-  taskName.setAttribute('name', 'TaskName');
-  taskName.setAttribute('placeholder', name);
-
-  const taskDesc = document.createElement('textarea');
-  taskDesc.setAttribute('type', 'text');
-  taskDesc.setAttribute('name', 'TaskDescription');
-  taskDesc.setAttribute('placeholder', desc);
-  taskDesc.setAttribute('rows', 5);
-
-  const taskProj = document.createElement('input');
-  taskProj.setAttribute('type', 'text');
-  taskProj.setAttribute('name', 'TaskProject');
-  taskProj.setAttribute('placeholder', project);
-
-  const taskStatus = document.createElement('input');
-  taskStatus.setAttribute('type', 'checkbox');
-  taskStatus.setAttribute('name', 'TaskStatus');
-  taskStatus.setAttribute('value', status);
-  taskStatus.id = 'task-status-checkbox';
-
-  const taskStatusLabel = document.createElement('label');
-  taskStatusLabel.classList.add('task-status-label');
-  taskStatusLabel.htmlFor = 'task-status-checkbox';
-  taskStatusLabel.innerHTML = 'Completed?';
-  taskStatusLabel.appendChild(taskStatus);
-
-  const formSubmit = document.createElement('input');
-  formSubmit.setAttribute('type', 'submit');
-  formSubmit.setAttribute('value', addEdit);
-
-  workspace.appendChild(overlay);
-  overlay.appendChild(card);
-  card.appendChild(cardForm);
-  cardForm.appendChild(taskName);
-  cardForm.appendChild(taskProj);
-  cardForm.appendChild(taskDesc);
-  cardForm.appendChild(taskStatusLabel);
-  cardForm.appendChild(formSubmit);
-
-  cardForm.addEventListener('submit', appendNewTask);
 }
